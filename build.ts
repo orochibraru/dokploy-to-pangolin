619d31e32b5dcf2d5f39649d65bbe0e070d1c314
Nicolas Boyer <boyer63nicolas@gmail.com>
2026-03-03 17:53:32 +0100

    refactor: structure

--- build.ts
+++ build.ts
@@ -1,11 +1,15 @@
-try {
-	console.log("Building the project...");
-	await Bun.build({
-		entrypoints: ["./index.ts"],
-		outdir: "./build",
-	});
-	console.log("Build completed successfully.");
-} catch (error) {
-	console.error("Build failed:", error);
-	process.exit(1);
+async function build() {
+	try {
+		console.log("Building the project...");
+		await Bun.build({
+			entrypoints: ["./src/index.ts"],
+			outdir: "./build",
+		});
+		console.log("Build completed successfully.");
+	} catch (error) {
+		console.error("Build failed:", error);
+		process.exit(1);
+	}
 }
+
+void build();
